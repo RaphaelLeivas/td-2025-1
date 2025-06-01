30697eaffb0d6e7eb877e3f041ca248a1b9519e3
Milton Bravo <bravocoluna0@gmail.com>
2025-06-01 16:08:37 -0300

    comitHVI

--- arquivos_tc/lEvalHVI.m
+++ arquivos_tc/lEvalHVI.m
@@ -2,7 +2,7 @@ clear all
 close all
 clc
 %=========================================================================
-CSVInput = 'ExResult.csv';
+CSVInput = 'resultados_pareto_somaPonderada_HVI.csv';
 %=========================================================================
 
 [HVI] = EvalParetoApp(CSVInput);
